62c84bc25b1a4068c69a4c05487bc5454d9f57b8
jamie7dev <jamie7dev@gmail.com>
2023-06-14 15:49:55 -0700

    refactor: top-down selector

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,12 +1,25 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+// bottom-up
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    // console.log(e.currentTarget.parentElement.parentElement);
-    const question = e.currentTarget.parentElement.parentElement;
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     // console.log(e.currentTarget.parentElement.parentElement);
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// top-down
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", function () {
     question.classList.toggle("show-text");
   });
 });
