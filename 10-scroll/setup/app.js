09894cdd2fe4f78abdb9a433de7f56197bd57d97
jamie7dev <jamie7dev@gmail.com>
2023-06-17 14:39:01 -0700

    feat: fixed-navbar when scroll

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -27,7 +27,21 @@ navToggle.addEventListener("click", function () {
   }
 });
 
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
 // ********** fixed navbar ************
+
+window.addEventListener("scroll", function () {
+  // console.log(window.pageYOffset);
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
