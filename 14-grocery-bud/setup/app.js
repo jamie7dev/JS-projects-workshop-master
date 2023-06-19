817cc6f510afed62ef53c84ff7678054a2e25fc0
jamie7dev <jamie7dev@gmail.com>
2023-06-19 12:45:37 -0700

    feat: add list, show list

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,10 +1,52 @@
 // ****** SELECT ITEMS **********
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
 
+// submit form
+form.addEventListener("submit", addItem);
+
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  // console.log(value, id);
+
+  if (value !== " " && !editFlag) {
+    const element = document.createElement("article");
+
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+    <!-- edit button -->
+    <button type="button" class="edit-btn">
+    <i class="fas fa-edit"></i>
+    </button>
+    <!-- delete btn -->
+    <button type="button" class="delete-btn">
+    <i class="fas fa-trash"></i>
+    </button>
+    </div>
+    `;
+    list.appendChild(element);
+    container.classList.add("show-container");
+  }
+}
 
 // ****** LOCAL STORAGE **********
 
