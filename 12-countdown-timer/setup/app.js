96e66874a09fb2f20f66f699e2dc91c6ecd3b8eb
jamie7dev <jamie7dev@gmail.com>
2023-06-18 18:46:10 -0700

    feat: dynamic date

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,16 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadLine = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+const tempDate = new Date();
+console.log(tempDate);
+const tempYear = tempDate.getFullYear();
+const tempMonth = tempDate.getMonth();
+const tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+console.log(futureDate);
